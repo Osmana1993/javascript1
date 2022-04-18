c7c4c81ab41a1d2d06804b48871553fe5e00c52b
amna <othmanalialamin@gmail.com>
2022-04-18 12:40:49 -0700

    yes

---
+++ Desktop/BootCamp/Challenes/challenge-4/js/questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the purpose of the alt attribute for images?",
+    answer: "To provide context for the image",
+    options: [
+      "To make the image load faster",
+      "To make it easier to style the image with CSS",
+      "To provide context for the image"
+    ]
+  },
+    {
+    numb: 4,
+    question: "By default, in which direction does a flexbox lay out its items?",
+    answer: "A row (horizontal), with all of the child elements laid out side by side ",
+    options: [
+        
+     "A row (horizontal), with all of the child elements laid out side by side ",
+     "A column (vertical), with all of the child elements laid out on top of one another.",
+     "A verticle ",
+     "inline"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How do you create a flexbox?",
+    answer: "display: flex",
+    options: [
+      "display: flex",
+      "display: flexbox",
+      "display: box",
+      "display: inline"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
